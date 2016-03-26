23a65500958d7611511fae201481b4a6ff299f54
danpouma <dpoumakis@yahoo.com>
2016-03-26 12:14:01 -0400

    added genre && recommendations classes

---
+++ src/engine/Genre.java
@@ -0,0 +1,62 @@
+package engine;
+
+/**
+ *
+ * @author dpoumakis
+ */
+public class Genre
+{
+    private int id;
+    private String name;
+    private String description;
+
+    public Genre()
+    {
+        id = 0;
+        name = "";
+        description = "";
+    }
+    
+    public Genre(int id, String name, String description)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Genre{" + "id=" + id + ", name=" + name + ", description=" + description + '}';
+    }
+}
---
+++ src/engine/Recommendations.java
@@ -0,0 +1,83 @@
+package engine;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author dpoumakis
+ */
+public class Recommendations
+{
+    private int id;
+    private int userId;
+    private LinkedList books;
+
+    public Recommendations(int id, int userId)
+    {
+        this.id = id;
+        this.userId = userId;
+        books = new LinkedList();
+    }
+    
+    public void addBook(Book book)
+    {
+        books.add(book);
+    }
+    
+    // Remove by index
+    public void removeBook(int index)
+    {
+        books.remove(index);
+    }
+    
+    // Remove by book
+    public void removeBook(Book book)
+    {
+        // Will need to wrap in conditions
+        books.remove(book);
+    }
+    
+    // Replace by index
+    public void replaceBook(int index)
+    {
+        // add logic from lab #4
+    }
+    
+    //Replace by book
+    public void replaceBook(Book book)
+    {
+        // add logic from lab #4
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public LinkedList getBooks()
+    {
+        return books;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+
+    public void setBooks(LinkedList books)
+    {
+        this.books = books;
+    }
+    
+    
+}
